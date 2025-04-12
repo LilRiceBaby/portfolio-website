22f78720aff1188f4435c3c5c04368eed49065e8
Robin Su <111165598+LilRiceBaby@users.noreply.github.com>
2025-04-12 18:26:50 -0500

    Update ProjectCard.js

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,15 +1,24 @@
 import { Col } from "react-bootstrap";
 
-export const ProjectCard = ({ title, description, imgUrl }) => {
+export const ProjectCard = ({ title, description, imgUrl, githubUrl }) => {
   return (
-    <Col size={12} sm={6} md={4}>
+    <Col xs="auto">
       <div className="proj-imgbx">
-        <img src={imgUrl} />
+        <div className="project-img-wrapper">
+          <img src={imgUrl} alt={title} className="project-image" />
+        </div>
         <div className="proj-txtx">
           <h4>{title}</h4>
           <span>{description}</span>
+          {githubUrl && (
+            <div className="github-link">
+              <a href={githubUrl} target="_blank" rel="noopener noreferrer">
+                View on GitHub ↗
+              </a>
+            </div>
+          )}
         </div>
       </div>
     </Col>
-  )
-}
+  );
+};
