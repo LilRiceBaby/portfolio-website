9092dcce1296803c238e894cef5cf13e43e5c37c
Robin Su <111165598+LilRiceBaby@users.noreply.github.com>
2025-04-18 00:38:44 -0500

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,7 @@ import { Navbar, Nav, Container } from "react-bootstrap";
 import logo from '../assets/img/alien-logo.png';
 import navIcon1 from '../assets/img/nav-icon1.svg';
 import navIcon2 from '../assets/img/nav-icon2.png';
-import navIcon3 from '../assets/img/nav-icon3.svg';
+import navIcon3 from '../assets/img/nav-icon3.png';
 import { HashLink } from 'react-router-hash-link';
 import {
   BrowserRouter as Router
@@ -52,7 +52,7 @@ export const NavBar = () => {
               <div className="social-icon">
                 <a href="https://www.linkedin.com/in/robin-su-aa7575186/"><img src={navIcon1} alt="https://www.linkedin.com/in/robin-su-aa7575186/"/></a>
                 <a href="https://github.com/LilRiceBaby?tab=overview&from=2025-04-01&to=2025-04-03"><img src={navIcon2} alt="https://github.com/LilRiceBaby?tab=overview&from=2025-04-01&to=2025-04-03" /></a>
-                <a href="https://www.instagram.com/lil_ether_/"><img src={navIcon3} alt="https://www.instagram.com/lil_ether_/" /></a>
+                <a href="https://www.youtube.com/shorts/5-oiLKEWIEw"><img src={navIcon3} alt="https://www.youtube.com/shorts/5-oiLKEWIEw" /></a>
               </div>
               <HashLink to='#connect'>
                 <button className="vvd"><span>Let’s Connect</span></button>
